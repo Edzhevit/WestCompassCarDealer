dc3dfe64754a6d729fb279ff121d4cb68b87bcf8
Edzhevit <nightwalker_@abv.bg>
2019-11-04 12:38:53 +0000

    Added service models

---
+++ src/main/java/domain/models/service/CarCreateServiceModel.java
@@ -0,0 +1,41 @@
+package models.service;
+
+public class CarCreateServiceModel {
+
+    private String brand;
+    private String model;
+    private String year;
+    private String engine;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+}
